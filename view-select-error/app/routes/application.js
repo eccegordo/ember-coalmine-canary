cc94fc5272969130513fa85caa3cb53d3201060e
Gordon Potter <eccegordo@gmail.com>
2014-11-07 23:25:34 -0800

    [view-select-error] ember docs are just wrong
    Its the details that matter. Canonical example of ember select does not seem to work.
    
    cf
    http://emberjs.com/api/classes/Ember.Select.html

--- view-select-error/app/routes/application.js
+++ view-select-error/app/routes/application.js
@@ -3,14 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model: function() {
-    var data = [    
-                  {foo: "foo 1", bar: "bar 1", id: 1},
-                  {foo: "foo 2", bar: "bar 2", id: 2},
-                  {foo: "foo 3", bar: "bar 3", id: 3},
-                  {foo: "foo 4", bar: "bar 4", id: 4},
-                  {foo: "foo 5", bar: "bar 5", id: 5},
-               ];
+    // var data = [    
+    //               {foo: "foo 1", bar: "bar 1", id: 1},
+    //               {foo: "foo 2", bar: "bar 2", id: 2},
+    //               {foo: "foo 3", bar: "bar 3", id: 3},
+    //               {foo: "foo 4", bar: "bar 4", id: 4},
+    //               {foo: "foo 5", bar: "bar 5", id: 5}
+    //            ];
+    // return data;
 
+    var data = [ "A", "B", "C", "D" ];
     return data;
+
   }
 });
